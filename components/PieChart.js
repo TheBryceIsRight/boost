ec7142e5877eb94c52b97359ce3ebc53ed9a0801
Bryce Watson <brycewatson315@gmail.com>
2021-04-17 20:50:59 -0400

    updated pie chart

--- components/PieChart.js
+++ components/PieChart.js
@@ -50,14 +50,14 @@ class Chart extends React.Component {
 
     render() {
         return (
-            <div className="chart" style={{height:400, minWidth:"60ch"}}>
+            <div className="chart" style={{height:400, minWidth:"100%", borderRadius:4}}>
                 <ResponsivePie
                     data={data}
                     margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
                     innerRadius={0.5}
                     padAngle={0.7}
                     cornerRadius={3}
-                    colors={{ scheme: 'pastel1' }}
+                    colors={{ scheme: 'pastel2' }}
                     borderWidth={1}
                     borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
                     radialLabelsSkipAngle={10}
@@ -65,76 +65,6 @@ class Chart extends React.Component {
                     radialLabelsLinkColor={{ from: 'color' }}
                     sliceLabelsSkipAngle={10}
                     sliceLabelsTextColor="#333333"
-                    defs={[
-                        {
-                            id: 'dots',
-                            type: 'patternDots',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            size: 4,
-                            padding: 1,
-                            stagger: true
-                        },
-                        {
-                            id: 'lines',
-                            type: 'patternLines',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            rotation: -45,
-                            lineWidth: 6,
-                            spacing: 10
-                        }
-                    ]}
-                    fill={[
-                        {
-                            match: {
-                                id: 'ruby'
-                            },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'c'
-                            },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'go'
-                            },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'python'
-                            },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'scala'
-                            },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'lisp'
-                            },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'elixir'
-                            },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'javascript'
-                            },
-                            id: 'lines'
-                        }
-                    ]}
                     legends={[
                         {
                             anchor: 'bottom',
@@ -167,7 +97,6 @@ class Chart extends React.Component {
                         height:50vh;
                         width:60vw;
                         background: white;
-                        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
                         transition: 0.3s;
                     }
                     
